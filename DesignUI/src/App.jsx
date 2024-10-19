59af7cac38143afc3244a8f61a5498ee0154d2e3
saptankshi <56836715+saptankshi@users.noreply.github.com>
2024-10-19 19:59:43 +0530

    Update App.jsx

--- DesignUI/src/App.jsx
+++ DesignUI/src/App.jsx
@@ -1,12 +1,20 @@
 import React, { useState, useEffect } from 'react';
+import MyComponent from './Home/MyComponent';
 import MainLayout from './Components/MainLayout';
 import './App.css'
 
 function App() {
+  const [currentPath, setCurrentPath] = useState(window.location.pathname);
+
+  useEffect(() => {
+    const handlePathChange = () => setCurrentPath(window.location.pathname);
+    window.addEventListener('popstate', handlePathChange);
+    return () => window.removeEventListener('popstate', handlePathChange);
+  }, []);
 
   return (
     <>
-   <MainLayout />
+      {currentPath === '/' ? <MyComponent /> : <MainLayout />}
     </>
   );
 }
